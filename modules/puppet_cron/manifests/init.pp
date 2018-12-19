89aad9ca110d8678102e8382ef5e4269516c2a3c
Boye Holden <boye.holden@ntnu.no>
2018-12-19 15:23:51 +0100

    Removed commentsCleanup

--- modules/puppet_cron/manifests/init.pp
+++ modules/puppet_cron/manifests/init.pp
@@ -1,22 +1,20 @@
 class puppet_cron {
 
-  // Let's make sure the Puppet daemon isn't running.
-  service { "puppet":
+  service { 'puppet':
     ensure => stopped,
     enable => false,
   } ->
 
-  // Pull the latest commits to our Puppet module repository every 4 minutes.
   cron { 'puppet-pull':
     ensure => present,
-    command => "cd /etc/puppetlabs/code/modules ; /usr/bin/git pull",
+    command => 'cd /etc/puppetlabs/code/modules ; /usr/bin/git pull',
     user => 'root',
     minute => '*/4',
   } ~>
 
   cron { 'puppet-apply':
     ensure => present,
-    command => "/opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/site.pp",
+    command => '/opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/site.pp',
     user => 'root',
     minute => '*/5',
   }
