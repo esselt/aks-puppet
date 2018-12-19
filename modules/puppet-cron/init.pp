f580d04177598965bad644df8c93b351612fa992
Boye Holden <boye.holden@ntnu.no>
2018-12-19 11:46:44 +0100

    Initial commit

---
+++ modules/puppet-cron/init.pp
@@ -0,0 +1,23 @@
+class puppet-cron (){
+
+  // Let's make sure the Puppet daemon isn't running.
+  service { "puppet":
+    ensure => stopped,
+    enable => false,
+  } ->
+
+  // Pull the latest commits to our Puppet module repository every 4 minutes.
+  cron { 'puppet-pull':
+    ensure => present,
+    command => "cd /etc/puppetlabs/code ; /usr/bin/git pull",
+    user => 'root',
+    minute => '*/4',
+  } ~>
+
+  cron { 'puppet-apply':
+    ensure => present,
+    command => "/opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/site.pp"
+    user => 'root',
+    minute => '*/5',
+  }
+}
