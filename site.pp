ca8b82571cab3692828a326f45c40b5d39449d81
Boye Holden <BOYH@equinor.com>
2018-12-20 13:19:55 +0100

    Missing colon

--- site.pp
+++ site.pp
@@ -4,39 +4,39 @@ node default {
   include auditd
 
   ## DOCKER RULES ##
-  auditd::rule { 'watch /usr/bin/docker'
+  auditd::rule { 'watch /usr/bin/docker':
     content => '-w /usr/bin/docker -p wa'
     order   => 101
   }
-  auditd::rule { 'watch /var/lib/docker'
+  auditd::rule { 'watch /var/lib/docker':
     content => '-w /var/lib/docker -p wa'
     order   => 102
   }
-  auditd::rule { 'watch /etc/docker'
+  auditd::rule { 'watch /etc/docker':
     content => '-w /etc/docker -p wa'
     order   => 103
   }
-  auditd::rule { 'watch /lib/systemd/system/docker.service'
+  auditd::rule { 'watch /lib/systemd/system/docker.service':
     content => '-w /lib/systemd/system/docker.service -p wa'
     order   => 104
   }
-  auditd::rule { 'watch /lib/systemd/system/docker.socket'
+  auditd::rule { 'watch /lib/systemd/system/docker.socket':
     content => '-w /lib/systemd/system/docker.socket -p wa'
     order   => 105
   }
-  auditd::rule { 'watch /etc/default/docker'
+  auditd::rule { 'watch /etc/default/docker':
     content => '-w /etc/default/docker -p wa'
     order   => 106
   }
-  auditd::rule { 'watch /etc/docker/daemon.json'
+  auditd::rule { 'watch /etc/docker/daemon.json':
     content => '-w /etc/docker/daemon.json -p wa'
     order   => 107
   }
-  auditd::rule { 'watch /etc/docker/daemon.json'
+  auditd::rule { 'watch /etc/docker/daemon.json':
     content => '-w /etc/docker/daemon.json -p wa'
     order   => 108
   }
-  auditd::rule { 'watch /usr/bin/docker-runc'
+  auditd::rule { 'watch /usr/bin/docker-runc':
     content => '-w /usr/bin/docker-runc -p wa'
     order   => 109
   }
