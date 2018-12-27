7f9cf77d81e921f39a3093e3c2f60b1899501413
Boye Holden <boye.holden@ntnu.no>
2018-12-27 09:16:22 +0100

    Added blacklisted modules and removed games user

--- site.pp
+++ site.pp
@@ -1,6 +1,33 @@
 node default {
   include puppet_cron
 
+  ## CLEAN UNNECESSARY USERS ##
+  user { 'games':
+    ensure => 'absent'
+  }
+
+  ## BLACKLIST KERNEL MODULES ##
+  $module_blacklist = @(EOT)
+    # Modules disabled per Azure baseline
+    install dccp /bin/true
+    install sctp /bin/true
+    install rds /bin/true
+    install tipc /bin/true
+    install cramfs /bin/true
+    install freevxfs /bin/true
+    install hfs /bin/true
+    install hfsplus /bin/true
+    install jffs2 /bin/true
+    install squashfs /bin/true
+    | EOT
+  file { '/etc/modprobe.d/blacklist-azurebaseline.conf':
+    ensure => file,
+    content => $module_blacklist,
+    owner => 'root',
+    group => 'root',
+    mode => '0644'
+  }
+
   ## DOCKER RULES ##
   include auditd
   auditd::rule { 'watch /usr/bin/docker':
