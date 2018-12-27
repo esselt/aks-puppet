a6735f2148d74745162f6b736ffb200d0e03a0af
Boye Holden <boye.holden@ntnu.no>
2018-12-27 13:14:37 +0100

    Only use Systemd lens and the specific file

--- site.pp
+++ site.pp
@@ -3,6 +3,8 @@ node default {
 
   ## DISABLE DOCKER ICC ##
   augeas { 'disable icc':
+    incl => '/etc/systemd/system/docker.service.d/exec_start.conf',
+    lens => 'Systemd.lns',
     context => '/files/etc/systemd/system/docker.service.d/exec_start.conf/Service',
     changes => "set ExecStart[ command = '/usr/bin/dockerd' ]/arguments/01 '--icc=false'",
     onlyif => "match ExecStart[ command = '/usr/bin/dockerd' ]/arguments/*[ . = '--icc=false' ] size == 0"
